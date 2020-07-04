05a2e57316498f08306e724a6da00889a6cd49b6
David Shilkret <dshilkret@gmail.com>
2020-07-03 20:01:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
     tenantUrl = localStorage.getItem("tenantUrl");
     //store config globally which will be useful for subsequent call.
     config = {
-        clientId: '86694b12-64bb-4f64-99d7-3adc1e6dde17', //57fbc2a2-1188-4ed0-aaf2-daca857d6957
+        clientId: '9624a483-5297-45f7-91da-a8ec0e75798b', //57fbc2a2-1188-4ed0-aaf2-daca857d6957
         postLogoutRedirectUri: 'https://tokendemo2.vercel.app/index.html',
         redirectUri:'https://tokendemo2.vercel.app/index.html',
         endpoints: {
